5e7438dc37ce51a724e757fb303410f90c91fb4a
Crystal Lee <crys@nyu.edu>
2019-08-01 18:03:10 -0400

    Implemented Google Maps API

--- src/App.js
+++ src/App.js
@@ -1,32 +1,15 @@
 
 import React, { Component } from "react";
-import logo from './logo.svg';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 import './App.css';
 
 
 class App extends Component{
-  constructor(props){
-  super(props);
-  this.state = { apiResponse: "" };
-}
-
-  callAPI(){
-    fetch("http://localhost:9000/testAPI")
-      .then(res => res.text())
-      .then(res => this.setState({ apiResponse: res}));
-  }
-  componentWillMount(){
-    this.callAPI();
-  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p className="App-intro"> {this.state.apiResponse} </p>
-        </header>
-      </div>
+      <Map google={this.props.google} zoom={12} initialCenter={{lat: 40.78, lng: -73.97}}/>
     );
   }
-}
-export default App;
+}export default GoogleApiWrapper({
+  apiKey: 'AIzaSyDE_CZbHCVVapxbJ92-Rui0bIl13Yhafx0'
+})(App);
